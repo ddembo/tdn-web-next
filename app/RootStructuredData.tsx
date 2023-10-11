054399878543f8acff7b4dc2912745ed2c655683
David Dembo <bootlegneurons@gmail.com>
2023-10-12 09:09:04 +1100

    add missing id link to root structured data

--- app/RootStructuredData.tsx
+++ app/RootStructuredData.tsx
@@ -38,7 +38,7 @@ const pageStructuredData: Graph = {
     {
       '@type': 'WebPage',
       '@id': RootPageId,
-      isPartOf: SiteNodeId,
+      isPartOf: { "@id": SiteNodeId },
       // NOTE: created issue #13 to improve datetime handling here
       // COULD potentially use a vscode keybinding or snippet too https://stackoverflow.com/questions/38780057/how-to-insert-current-date-time-in-vscode
       dateCreated: '2023-09-21T20:39:00+11:00',
