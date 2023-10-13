6c04d4e2041a3135832b8db9286fab638111db51
David Dembo <bootlegneurons@gmail.com>
2023-10-13 15:33:17 +1100

    make PromptLink accept href

--- components/PromptLink/PromptLink.tsx
+++ components/PromptLink/PromptLink.tsx
@@ -3,10 +3,16 @@
 import Link from 'next/link';
 import type { ReactNode } from 'react';
 
-export default function PromptLink({ prompt, children }: { prompt: string; children: ReactNode }) {
+interface PromptLinkProps {
+  prompt: string;
+  children: ReactNode;
+  href?: string;
+}
+
+export default function PromptLink({ href = '/', prompt, children }: PromptLinkProps) {
   return (
     <Link
-      href="/"
+      href={href}
       className="self-center font-light text-sm no-underline hover:underline"
       onClick={(event) => {
         const isConfirmed = confirm(prompt);
