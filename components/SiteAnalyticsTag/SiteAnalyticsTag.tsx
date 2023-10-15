48eae6ded608465e46fd3f517af54f474eb374ff
David Dembo <bootlegneurons@gmail.com>
2023-10-15 19:16:10 +1100

    lazy load GA script

--- components/SiteAnalyticsTag/SiteAnalyticsTag.tsx
+++ components/SiteAnalyticsTag/SiteAnalyticsTag.tsx
@@ -7,12 +7,12 @@ export default function SiteAnalyticsTag() {
   return (
     <>
       <Script
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
